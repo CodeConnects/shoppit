b6c46e34afc79c51135c7684d1db5c0a2583214f
Scott Cielo <codeconnects@gmail.com>
2018-12-26 11:46:49 -0500

    filled out "add new note" functionality

--- notes.js
+++ notes.js
@@ -1,7 +1,30 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-    console.log('Adding note', '"'+title+'", ', '"'+body+'"');
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    var duplicateNotes = notes.filter( (note) => note.title === title );
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes) );
+        console.log('Added note', '"'+title+'", ', '"'+body+'"');
+    } else {
+        console.log('A note with the title of '+title+' already exists. Please add your note again using a different title.');
+    }
 }
 
 var getAll = () => {
