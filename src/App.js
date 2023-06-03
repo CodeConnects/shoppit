2a4604bec9c26bfa013124cba652c266bcf3e3dc
CodeConnects <codeconnects@gmail.com>
2023-06-03 11:11:44 -0400

    map wrappers with an array of categories

--- src/App.js
+++ src/App.js
@@ -1,49 +1,42 @@
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: "Category",
+    },
+    {
+      id: 2,
+      title: "Category",
+    },
+    {
+      id: 3,
+      title: "Category",
+    },
+    {
+      id: 4,
+      title: "Category",
+    },
+    {
+      id: 5,
+      title: "Category",
+    },
+  ];
+
   return (
     <div className="categories-wrap">
-
-      <div className="single-category">
-        <img src="https://via.placeholder.com/150" alt="category" />
-        <div className="single-category-body">
-          <h3>Category 1</h3>
-          <p>shoppit</p>
+      {categories.map((category) => (
+        <div className="single-category" key={category.id}>
+          <img src="https://via.placeholder.com/150" alt="category" />
+          <div className="single-category-body">
+            <h3>{category.title} {category.id}</h3>
+            <p>shoppit</p>
+          </div>
         </div>
-      </div>
-
-      <div className="single-category">
-        <img src="https://via.placeholder.com/150" alt="category" />
-        <div className="single-category-body">
-          <h3>Category 2</h3>
-          <p>shoppit</p>
-        </div>
-      </div>
-
-      <div className="single-category">
-        <img src="https://via.placeholder.com/150" alt="category" />
-        <div className="single-category-body">
-          <h3>Category 3</h3>
-          <p>shoppit</p>
-        </div>
-      </div>
-
-      <div className="single-category">
-        <img src="https://via.placeholder.com/150" alt="category" />
-        <div className="single-category-body">
-          <h3>Category 4</h3>
-          <p>shoppit</p>
-        </div>
-      </div>
-
-      <div className="single-category">
-        <img src="https://via.placeholder.com/150" alt="category" />
-        <div className="single-category-body">
-          <h3>Category 5</h3>
-          <p>shoppit</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
