3e05003d368e069cd1ba73719d94246327f036fd
CodeConnects <codeconnects@gmail.com>
2023-06-03 10:54:51 -0400

    add basic categories html structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 
 const App = () => {
   return (
-    <div className="App">
-      <h2>Shoppit</h2>
+    <div className="categories-wrap">
+
+      <div className="single-category">
+        <img src="https://via.placeholder.com/150" alt="category" />
+        <div className="single-category-body">
+          <h3>Category 1</h3>
+          <p>shoppit</p>
+        </div>
+      </div>
+
+      <div className="single-category">
+        <img src="https://via.placeholder.com/150" alt="category" />
+        <div className="single-category-body">
+          <h3>Category 2</h3>
+          <p>shoppit</p>
+        </div>
+      </div>
+
+      <div className="single-category">
+        <img src="https://via.placeholder.com/150" alt="category" />
+        <div className="single-category-body">
+          <h3>Category 3</h3>
+          <p>shoppit</p>
+        </div>
+      </div>
+
+      <div className="single-category">
+        <img src="https://via.placeholder.com/150" alt="category" />
+        <div className="single-category-body">
+          <h3>Category 4</h3>
+          <p>shoppit</p>
+        </div>
+      </div>
+
+      <div className="single-category">
+        <img src="https://via.placeholder.com/150" alt="category" />
+        <div className="single-category-body">
+          <h3>Category 5</h3>
+          <p>shoppit</p>
+        </div>
+      </div>
     </div>
   );
 }
