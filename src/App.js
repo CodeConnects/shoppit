57ea6eff0be8a81486553e233379bb2c6697b9cd
CodeConnects <codeconnects@gmail.com>
2023-12-04 14:50:52 -0500

    add category details and bg image urls

--- src/App.js
+++ src/App.js
@@ -5,37 +5,49 @@ const App = () => {
   const categories = [
     {
       id: 1,
-      title: "Category",
+      title: 'Handy Man Services',
+      image: 'https://www.doola.com/wp-content/uploads/2023/03/how-to-start-a-handy-man-business-1600x800.jpg',
     },
     {
       id: 2,
-      title: "Category",
+      title: 'Property Management',
+      image: 'https://lirp.cdn-website.com/f0370a60/dms3rep/multi/opt/fairview+property+management-1920w.jpeg',
     },
     {
       id: 3,
-      title: "Category",
+      title: 'Hard Money Lending',
+      image: 'https://bradloans.com/wp-content/uploads/2020/04/What-is-a-Hard-Money-Loan.jpg',
     },
     {
       id: 4,
-      title: "Category",
+      title: 'Residential Rehabs',
+      image: 'https://www.news10.com/wp-content/uploads/sites/64/2021/03/8504413673_e35f4bc5be_o.jpg',
     },
     {
       id: 5,
-      title: "Category",
+      title: 'Commercial Rehabs',
+      image: 'https://www.ocfrealty.com/wp-content/uploads/2023/05/My-project-2-2.jpg',
     },
   ];
 
   return (
-    <div className="categories-wrap">
+    <div className='categories-wrap'>
+      
       {categories.map((category) => (
-        <div className="single-category" key={category.id}>
-          <div className="category-bg-img" />
-          <div className="single-category-body">
-            <h3>{category.title} {category.id}</h3>
-            <p>shoppit</p>
+        
+        <div key={category.id} className='single-category'>
+          <div className='category-bg-img'  style={{
+            backgroundImage: `url(${category.image})`
+          }} />
+          
+          <div className='single-category-body'>
+            <h3>{category.title}</h3>
+            <p>lorem ipsum dolor sit amet, consectetur.</p>
           </div>
         </div>
+
       ))}
+  
     </div>
   );
 };
