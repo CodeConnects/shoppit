e02cd0b46004fcb95cc38b8e2f8c392e525f6060
CodeConnects <codeconnects@gmail.com>
2023-12-04 16:38:42 -0500

    Add filler Navbar and Routes for 3 pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
-import Catalog from './components/catalog/catalog.jsx';
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
+
+import Home from './routes/home/home.jsx';
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+const Cart = () => {
+  return (
+    <div>
+      <h1>Shopping Cart</h1>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+}
 
 const App = () => {
   return (
-    <Catalog />
+    <Routes>
+      <Route path="/" element={ <Navbar /> }>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/cart" element={ <Cart /> } />
+        <Route path="/about" element={ <About /> } />
+      </Route>
+    </Routes>
   );
 };
 
