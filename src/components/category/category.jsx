fbd02282b9632b6f15cfaeac921299222e8bdbcf
CodeConnects <codeconnects@gmail.com>
2023-12-04 15:07:58 -0500

    break out Category into separate component

---
+++ src/components/category/category.jsx
@@ -0,0 +1,16 @@
+import './category.scss';
+
+const Category = ({ title, image }) => (
+  <div className='single-category'>
+    <div className='category-bg-img'  style={{
+      backgroundImage: `url(${image})`
+    }} />
+    
+    <div className='single-category-body'>
+      <h3>{title}</h3>
+      <p>lorem ipsum dolor sit amet, consectetur.</p>
+    </div>
+  </div>
+);
+
+export default Category;
