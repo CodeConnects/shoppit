a9d2439dcd474f77f23b591725c26acc71a650e2
CodeConnects <codeconnects@gmail.com>
2023-12-05 10:05:40 -0500

    pull out Navbar to its own component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
+import Navbar from './routes/Navbar/Navbar.jsx';
 import Home from './routes/Home/Home.jsx';
-
-const Navbar = () => {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
-    </div>
-  );
-}
 
 const Cart = () => {
   return (
